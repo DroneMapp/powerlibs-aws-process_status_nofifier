bfa3ec22ae497f0bf1540351da02bdcb8836214e
Cléber Zavadniak <cleber@dronemapp.com>
2019-12-05 17:33:11 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,11 @@
 import re
 from setuptools import setup
 
-VERSION = '0.1.1'
+VERSION = '0.2.0'
 
 
-def pip_git_to_setuptools_git(url):
-    match = re.match(
-        r'git\+https://github.com/(?P<owner>[^/]+)/(?P<repository>[^/]+).git@(?P<tag>.+)',
-        url
-    )
-    if match:
-        group_dict = match.groupdict()
-        url = "git+https://github.com/{owner}/{repository}.git@{tag}#egg={repository}-{tag}".format(
-            **group_dict
-        )
-        package = '{repository}=={tag}'.format(**group_dict)
-    return package, url
-
-
-def parse_requirements():
-    requires = []
-    dependency_links = []
-
-    with open('requirements/production.txt') as requirements_file:
-        for line in requirements_file:
-            if 'git+' in line:
-                package, link = pip_git_to_setuptools_git(line.strip())
-                dependency_links.append(link)
-            else:
-                package = line.strip()
-            requires.append(package)
-
-    return {'install_requires': requires, 'dependency_links': dependency_links}
-
+with open('requirements/production.txt') as requirements_file:
+    requires = [item for item in requirements_file]
 
 with open('README.md') as f:
     readme = f.read()
@@ -51,7 +24,11 @@ setup(
     author_email='adolfo.w.s@gmail.com',
     url='https://github.com/DroneMapp/powerlibs-aws-process_status_nofifier',
     license=package_license,
-    packages=['powerlibs', 'powerlibs.aws', 'powerlibs.aws.process_status_notifier'],
+    packages=[
+        'powerlibs',
+        'powerlibs.aws',
+        'powerlibs.aws.process_status_notifier'
+    ],
     package_data={'': ['LICENSE', 'README.md']},
     include_package_data=True,
     zip_safe=False,
@@ -64,5 +41,5 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ),
-    **parse_requirements()
+    install_requires=requires
 )
